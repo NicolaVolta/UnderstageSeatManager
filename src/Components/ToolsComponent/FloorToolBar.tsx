72b97aace142b48412009b20c2bcceb19f3666d0
NicolaVolta <nvoltarel@gmail.com>
2022-09-29 13:03:07 +0200

    Floor management
    Added floor add and remove management
    Added floor selection management

--- src/Components/ToolsComponent/FloorToolBar.tsx
+++ src/Components/ToolsComponent/FloorToolBar.tsx
@@ -1,29 +1,80 @@
-import { IconButton, Container, Stack, FlexboxGrid, Button } from "rsuite";
+import { IconButton, Container, Stack, FlexboxGrid, Button, Modal } from "rsuite";
 import './styling/FloorToolBarStyle.css';
 import { BsFillTrashFill, BsStack } from 'react-icons/bs';
 import { AiOutlinePlus } from 'react-icons/ai';
+import { useState, useEffect } from 'react';
 
-export function FloorToolBar(){
+interface FloorToolBarProps {
+    totalFloors : number,
+    handleFloorAddRemove : (isAdd : boolean) => void,
+    handleFloorSelection : (newFloor : number) => void,
+}
+
+export function FloorToolBar({ totalFloors, handleFloorAddRemove, handleFloorSelection } : FloorToolBarProps){
+    const [showConfirmDelete, setShowConfirmDelete] = useState<boolean>(false);
+    const [currentFloor, setCurrentFloor] = useState<number>(0);
+    const [totalFloorNumber, setTotalFloorNumber] = useState<number>(totalFloors);
+
+    const handleFloorAddRemoveButtons = (isAdd : boolean) => {
+        if(showConfirmDelete) setShowConfirmDelete(false);
+
+        let newFloorNumber=totalFloorNumber===1 && !isAdd ? 1 : totalFloorNumber + (isAdd ? 1 : -1);
+        let newCurrentFloor=totalFloorNumber===1 || currentFloor===0 ? 0 : currentFloor-1;
+
+        handleFloorAddRemove(isAdd);
+        handleFloorSelectionButton(newCurrentFloor);
+
+        setTotalFloorNumber(newFloorNumber);
+        setCurrentFloor(newCurrentFloor);         
+    }
+
+    const handleFloorSelectionButton = (index : number) => {
+        setCurrentFloor(index);
+        handleFloorSelection(index);
+    }
+
     return (
-        <Container className="floorToolBar">
-            <FlexboxGrid justify="space-between" align="bottom">
-                <FlexboxGrid.Item>
-                    <Stack direction="column-reverse" spacing={10} className="content">
-                        <BsStack size="2em"/>
-                        <Stack direction="column-reverse" spacing={1} className="floorContainer">
-                            <Button appearance="subtle" color="blue" className="floorButton">1</Button>
-                            <Button appearance="subtle" color="blue" className="floorButton">2</Button>
-                            <Button appearance="subtle" color="blue" className="floorButton">3</Button>
+        <>
+            <Container className="floorToolBar">
+                <FlexboxGrid justify="space-between" align="bottom">
+                    <FlexboxGrid.Item>
+                        <Stack direction="column-reverse" spacing={10} className="content">
+                            <BsStack size="2em"/>
+                            <Stack direction="column-reverse" spacing={1} className="floorContainer">
+                                {[...Array(totalFloorNumber)].map((el, index)=> {
+                                    return <Button key={index} 
+                                    appearance="ghost" 
+                                    color={currentFloor===index ? "yellow" : "blue"}
+                                    className="floorButton"
+                                    onClick={() => handleFloorSelectionButton(index)}>{index+1}</Button>
+                                })}
+                            </Stack>
                         </Stack>
-                    </Stack>
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item>
-                    <Stack direction="column-reverse" spacing={10} className="buttons">
-                        <IconButton icon={<BsFillTrashFill size="1.3em"/>} className="singleButton" appearance="primary" color="red" circle/>
-                        <IconButton icon={<AiOutlinePlus size="1.3em"/>} className="singleButton" appearance="primary" circle/>
-                    </Stack>
-                </FlexboxGrid.Item>
-            </FlexboxGrid>
-        </Container>
+                    </FlexboxGrid.Item>
+                    <FlexboxGrid.Item>
+                        <Stack direction="column-reverse" spacing={10} className="buttons">
+                            <IconButton icon={<BsFillTrashFill size="1.3em"/>} className="singleButton" appearance="primary" color="red" circle onClick={() => setShowConfirmDelete(true)}/>
+                            <IconButton icon={<AiOutlinePlus size="1.3em"/>} className="singleButton" appearance="primary" circle onClick={() => handleFloorAddRemoveButtons(true)}/>
+                        </Stack>
+                    </FlexboxGrid.Item>
+                </FlexboxGrid>
+            </Container>
+            <Modal open={showConfirmDelete} onClose={() => setShowConfirmDelete(false)}>
+                <Modal.Header>
+                    <Modal.Title>Conferma eliminazione</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Sicuro di voler eliminare il piano n° {currentFloor+1}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => setShowConfirmDelete(false)} appearance="subtle">
+                        Annulla
+                    </Button>
+                    <Button onClick={() => handleFloorAddRemoveButtons(false)} appearance="primary" color="red">
+                        Cancella
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
--- src/Pages/SeatManager.tsx
+++ src/Pages/SeatManager.tsx
@@ -14,14 +14,23 @@ import { PositionType } from '../Support/types/GenericTypes';
 import { SeatGroup } from '../FabricComponents/ComplexComponent/SeatGroup';
 import { MAX_ZOOM_IN, MAX_ZOOM_OUT } from '../Support/constants';
 
-export class SeatManager extends Component <{}, { selectedTool : ToolSelectionEnum, selectedItem : BaseItem | undefined, showContextMenu : boolean, contextMenuAnchorPoint : PositionType, currentZoom : number}>{
+interface State{
+    selectedTool : ToolSelectionEnum, 
+    selectedItem : BaseItem | undefined, 
+    showContextMenu : boolean, 
+    contextMenuAnchorPoint : PositionType, 
+    currentZoom : number,
+}
+
+export class SeatManager extends Component <{}, State>{
     private canvasRef : RefObject<HTMLCanvasElement>;
     private canvas : Canvas;
     private newElementCreating : BaseItem | undefined;
     private isMoving : boolean = false;
     private lastPositionInCanvas : PositionType | undefined;
-    private placeMap : Array<BaseItem> = [];
-    
+    private placeMap : Map<number, Array<BaseItem>>;
+    private selectedFloor : number = 0;
+
     constructor(props:any){
         super(props);
         this.canvasRef=createRef<HTMLCanvasElement>();
@@ -33,6 +42,9 @@ export class SeatManager extends Component <{}, { selectedTool : ToolSelectionEn
             contextMenuAnchorPoint: { x:0, y:0 },
             currentZoom:1,
         }
+
+        this.placeMap=new Map();
+        this.placeMap.set(0, []);
     }
     
     componentDidMount(): void {
@@ -145,7 +157,7 @@ export class SeatManager extends Component <{}, { selectedTool : ToolSelectionEn
 
         if(selectedObjects.length!==1) return;
 
-        let selectedItemArray : Array<BaseItem> = this.placeMap.filter((currentItem) => {
+        let selectedItemArray : Array<BaseItem> = this.placeMap.get(this.selectedFloor)!.filter((currentItem) => {
             return currentItem.getElement===selectedObjects[0];
         });
 
@@ -159,7 +171,11 @@ export class SeatManager extends Component <{}, { selectedTool : ToolSelectionEn
     }
 
     handleFinishCreation = (figureCreated : BaseItem) :void => {
-        this.placeMap.push(figureCreated);
+        let floorElements=this.placeMap.get(this.selectedFloor);
+
+        if(floorElements===undefined) floorElements=[];
+        
+        this.placeMap.set(this.selectedFloor, [...floorElements, figureCreated]);
         this.handleToolSelection(this.state.selectedTool);
     }
 
@@ -216,13 +232,50 @@ export class SeatManager extends Component <{}, { selectedTool : ToolSelectionEn
         if(this.state.currentZoom!==zoomLevel) this.setState({currentZoom : zoomLevel});
     }
 
+    handleFloorAddRemove = (isAdd : boolean) => {
+        if(isAdd)
+            this.placeMap.set(this.placeMap.size, []);
+        else {
+            if(this.placeMap.size===1) return this.placeMap.set(0, []);
+
+            this.placeMap.delete(this.selectedFloor);
+
+            let copyMap=new Map();
+
+            let keysIterator=this.placeMap.keys();
+            for(let i=0; i<this.placeMap.size; i++){
+                let currentKey=keysIterator.next().value;
+                copyMap.set(i, this.placeMap.get(currentKey) || []);
+            }
+
+            this.placeMap=copyMap;
+        }
+    }
+
+    handleFloorSelection = (newFloor : number) => {
+        this.canvas.clear();
+        this.selectedFloor=newFloor;
+
+        this.loadFloor();
+    }
+
+    loadFloor = () =>{
+        let itemsToLoad=this.placeMap.get(this.selectedFloor);
+
+        if(Utils.isNullOrUndefined(itemsToLoad)) return;
+
+        itemsToLoad!.forEach((currentItem) => {
+            this.canvas.add(currentItem.getElement);
+        });
+    }
+
     render(): ReactNode {
         return (
             <>
                 <ToolMenu handleSelection={this.handleToolSelection} selectedTool={this.state.selectedTool}/>
                 <EditItemControls selectedItem={this.state.selectedItem}/>
                 <ZoomBar currentZoom={this.state.currentZoom} handleZoomChange={this.handleZoomChange}/>
-                <FloorToolBar/>
+                <FloorToolBar totalFloors={this.placeMap.size} handleFloorAddRemove={this.handleFloorAddRemove} handleFloorSelection={this.handleFloorSelection}/>
                 <canvas id = 'canvas' ref={this.canvasRef}/>
                 {this.state.showContextMenu && <ContextMenu position={this.state.contextMenuAnchorPoint}/>}
             </>
